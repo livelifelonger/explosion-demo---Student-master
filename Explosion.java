de9fbe5772e3a7a02f4c7a5a007d063de0c658b7
livelifelonger <livelifelonger@users.noreply.github.com>
2017-12-12 14:55:16 -0600

    Initial commit

---
+++ Explosion.java
@@ -0,0 +1,49 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
+
+/**
+ * An explosion. It starts by expanding and then collapsing. 
+ * The explosion will explode other obejcts that the explosion intersects.
+ * 
+ * @author Poul Henriksen
+ * @version 1.0.1
+ */
+public class Explosion extends Actor
+{
+    /** How many images should be used in the animation of the explostion */
+    
+    
+    /** 
+     * The images in the explosion. This is static so the images are not
+     * recreated for every object (improves performance significantly).
+     */
+    
+    
+    /** Current size of the explosion */
+    
+    
+    /** How much do we increment the index in the explosion animation. */
+    
+    /**
+     * Create a new explosion.
+     */
+    public Explosion() 
+    {
+        
+    }    
+    
+    /** 
+     * Create the images for explosion.
+     */
+    public synchronized static void initializeImages() 
+    {
+        
+    }
+    
+    /**
+     * Explode!
+     */
+    public void act()
+    {
+        
+    }
+}
